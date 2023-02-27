3d17f2af0503303b218f41ec8ee5f81c5045ccc1
Aakash Kumar Saini <aakashkumarsaini.code@gmail.com>
2023-02-28 01:28:15 +0530

    lesson 2 app and jsx

---
+++ lesson_02/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // const name = "Aakash Kumar Saini";
+  const handelNameChange = () => {
+    const names = ['Avneet', 'Tanya', 'Srishti'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Aakash is fucking {handelNameChange()}.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
