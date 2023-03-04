e011735f4670101428bef6ed05d17e21427e7cec
Aakash Kumar Saini <aakashkumarsaini.code@gmail.com>
2023-03-04 23:51:53 +0530

    useStateHook

---
+++ lesson_06/src/Content.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+const Content = () => {
+  const [name, setName] = useState("Aakash");
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ["Aakash", "Avneet", "Tanya", "Srishti"];
+    const int = Math.floor(Math.random() * 4);
+    setName(names[int]);
+  };
+  const handleClick = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
+  };
+  const handleClick2 = () => {
+    console.log(count);
+  };
+  // const handleClick3 = (e) => {
+  //   console.log(e.target.innerText);
+  // };
+
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hello {name}.</p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
+    </main>
+  );
+};
+
+export default Content;
