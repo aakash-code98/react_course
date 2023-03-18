a77c158d681ad73f35663e137ee643b23a859252
Aakash Kumar Saini <aakashkumarsaini.code@gmail.com>
2023-03-19 01:31:30 +0530

    fetch API data

---
+++ lesson_13/src/SearchItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SearchItem = ({ search, setSearch }) => {
+  return (
+    <form
+      className="searchForm"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <label htmlFor="search">Search</label>
+      <input
+        name="search"
+        id="search"
+        type="text"
+        role="searchbox"
+        placeholder="Search Items"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchItem;
