6ed4185e0b29cba1dd4346c857a2d2428a049f64
Aakash Kumar Saini <aakashkumarsaini.code@gmail.com>
2023-04-05 03:07:47 +0530

    removed unused imports

--- lesson_16/src/App.js
+++ lesson_16/src/App.js
@@ -6,9 +6,7 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Routes, useHistory } from "react-router-dom";
-import { useState, useEffect } from "react";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
